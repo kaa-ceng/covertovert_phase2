da60ebd7627f9451316e292f8b75363123415627
kaa-ceng <knkarakut@gmail.com>
2024-12-23 00:39:40 +0300

    begin

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,25 +1,80 @@
+import time
+from scapy.all import IP, TCP, send, sniff
 from CovertChannelBase import CovertChannelBase
 
 class MyCovertChannel(CovertChannelBase):
-    """
-    - You are not allowed to change the file name and class name.
-    - You can edit the class in any way you want (e.g. adding helper functions); however, there must be a "send" and a "receive" function, the covert channel will be triggered by calling these functions.
-    """
-    def __init__(self):
+    def send(self, receiver_ip, receiver_port, sender_port, short_delay, long_delay, message_length, log_file):
         """
-        - You can edit __init__.
+        Send a covert message using timing delays between packet bursts.
+
+        Parameters:
+        receiver_ip: The IP address of the receiver.
+        receiver_port: The port the receiver listens on.
+        sender_port: The port the sender sends from.
+        short_delay: Delay (in seconds) representing binary '0'.
+        long_delay: Delay (in seconds) representing binary '1'.
+        message_length: Length of the binary message to send.
+        log_file: Path to log the sent message.
         """
-        pass
-    def send(self, log_file_name, parameter1, parameter2):
+        # Generate a random binary message
+        message = ''.join(self.get_random_message(message_length))
+
+        # Log the generated message
+        with open(log_file, "w") as log:
+            log.write(message)
+
+        print(f"Sending message: {message}")
+
+        # Send packets with appropriate delays
+        for bit in message:
+            pkt = IP(dst=receiver_ip) / TCP(dport=receiver_port, sport=sender_port)
+            send(pkt, verbose=False)
+            # Delay based on the bit value
+            time.sleep(short_delay if bit == '0' else long_delay)
+
+        # Send a termination packet (optional, for synchronization)
+        send(IP(dst=receiver_ip) / TCP(dport=receiver_port, sport=sender_port, flags="F"), verbose=False)
+        print("Message sent!")
+
+    def receive(self, sender_ip, sender_port, receiver_ip, receiver_port, short_delay, long_delay, log_file):
         """
-        - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        Receive a covert message by decoding timing delays between packet bursts.
+
+        Parameters:
+        sender_ip: The IP address of the sender.
+        sender_port: The port the sender sends from.
+        receiver_ip: The IP address of the receiver.
+        receiver_port: The port the receiver listens on.
+        short_delay: Threshold for '0'.
+        long_delay: Threshold for '1'.
+        log_file: Path to log the received message.
         """
-        binary_message = self.generate_random_binary_message_with_logging(log_file_name)
-        
-    def receive(self, parameter1, parameter2, parameter3, log_file_name):
-        """
-        - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
-        - After the implementation, please rewrite this comment part to explain your code basically.
-        """
-        self.log_message("", log_file_name)
+        print("Listening for packets...")
+        message = []
+        previous_time = None
+
+        def packet_handler(pkt):
+            nonlocal previous_time
+            current_time = time.time()
+
+            if previous_time is not None:
+                # Calculate time difference between consecutive packets
+                idle_time = current_time - previous_time
+                # Decode the bit based on idle_time
+                if idle_time < (short_delay + long_delay) / 2:
+                    message.append('0')
+                else:
+                    message.append('1')
+
+            previous_time = current_time
+
+        # Sniff packets with filter for sender/receiver communication
+        sniff(filter=f"tcp and host {sender_ip} and port {sender_port}",
+              prn=packet_handler, timeout=30)
+
+        # Log the received message
+        decoded_message = ''.join(message)
+        with open(log_file, "w") as log:
+            log.write(decoded_message)
+
+        print(f"Received message: {decoded_message}")
